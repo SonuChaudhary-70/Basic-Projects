6b070da8f51fb1e38f442cd4ac60266f368df4ea
Sonu Chaudhary <siddhantc982@gmail.com>
2022-10-28 11:33:04 +0530

    Update Email_Slicer-Project.py

--- Email Slicer/Email_Slicer-Project.py
+++ Email Slicer/Email_Slicer-Project.py
@@ -4,23 +4,19 @@
 email = input("Enter your Email :").strip()   # Get the email as input from the user AND strip() Method removes white spaces in beginning and at the end of the email if any.
 sym = '@'
 if sym in email:
-    k = email.split('@')                   # split() method split the email from @ into two parts first is a username and the other is a domain
-    print(f'\nUsername : {k[0]} \nDomain : {k[1]}')
+    splitedEmail = email.split('@')                   # split() method split the email from @ into two parts first is a username and the other is a domain
+    print(f'\nUsername : {splitedEmail[0]} \nDomain : {splitedEmail[1]}')
 else:
     print("Invalid email")
-# Mehod 2 (Already Defined Logic)             
+    
+# Method 2 (Already Defined Logic)             
                                             
 # email = input("Enter Your Email: ").strip() 
 # username = email[:email.index('@')]    -------> # It willl not print anything we have to add two print lines for Username and Domain
 # domain = email[email.index('@') + 1:]       
                                             
-# Mrehod 3  ( Modified logic of Method 2)
 
-email = input("\nEnter Your Email: ").strip()
-print(f"\nUsername : {email[:email.index('@')]}")
-print(f"Domain : {email[email.index('@') + 1:]}\n")
-
-# Another version of METHOD 3
+# Another version of METHOD 2
 
 email = input("\nEnter Your Email: ").strip()
 print(f"\nUsername : {email[:email.index('@')]}\nDomain : {email[email.index('@') + 1:]}\n")
